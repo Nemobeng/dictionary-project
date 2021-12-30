018863438737178860051a2ecf718a7d16b3320d
Nemobeng <obengnermine@gmail.com>
2021-12-30 13:24:32 +0000

    fixing failed deploy due to alt tag

--- src/Photos.js
+++ src/Photos.js
@@ -15,7 +15,7 @@ export default function Photos(props) {
                   href={photo.src.original}
                   target="_blank"
                   rel="noreferrer"
-                  alt="Search results"
+                  alt=""
                 >
                   <img src={photo.src.landscape} className="img-fluid" />
                 </a>
